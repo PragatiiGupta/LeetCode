e86ea0cd7ed7587aca226843c628f824263ced0e
Pragati Gupta <50272471+PragatiiGupta@users.noreply.github.com>
2024-06-18 11:47:10 +0530

    Update First_non-repeating_char.cpp

--- First_non-repeating_char.cpp
+++ First_non-repeating_char.cpp
@@ -1,3 +1,4 @@
+/**
 First non-repeating character in a stream.
 Given an input stream A of n characters consisting only of lower case alphabets. While reading characters from the stream, you have to tell which character has appeared only once in the stream upto that point. If there are many characters that have appeared only once, you have to tell which one of them was the first one to appear. If there is no such character then append '#' to the answer.
 
